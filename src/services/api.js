d001e038060ae998e5036c1756af30a66d0e80be
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-03-27 19:40:54 -0300

    changing expo ip to heroku url on api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // ip expo
-    baseURL: 'http://192.168.0.10:3333'
+    baseURL: 'https://semana-omnistack-backend.herokuapp.com/'
 });
 
 export default api;
